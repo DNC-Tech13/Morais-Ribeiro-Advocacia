d55f5a73092aaf187a6672b11f0dbb530d02f636
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-04-24 17:21:31 -0300

    inclusao do componente Header contendo navbar responsiv

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,14 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { Button, Card } from "react-bootstrap";
-
+import Header from "@/components/header/header";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      <header>
+        <Header/>
+      </header>
+      
     </main>
   );
 }
