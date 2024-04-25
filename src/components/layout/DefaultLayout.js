a49b32526ea07e9aa8175a09ddef44b1c2c88cd8
xTirian <tirianx@yahoo.com>
2024-04-25 19:33:06 -0300

    feat: criar layout default para as paginas com o header

---
+++ src/components/layout/DefaultLayout.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "@/components/header/header";
+import { ThemeProvider } from "react-bootstrap";
+import "@/styles/globals.scss";
+
+export default function Layout({ children }) {
+  return (
+    <ThemeProvider breakpoints={["xl", "md"]} minBreakpoint="xxs">
+      <header>
+        <Header />
+      </header>
+      {children}
+    </ThemeProvider>
+  );
+}
