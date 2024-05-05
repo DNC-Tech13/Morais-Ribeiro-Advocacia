be9df62327a1fc0a369b830cd2c2dea4e727830d
xTirian <tirianx@yahoo.com>
2024-05-05 02:34:52 -0300

    feat: button custom contato

---
+++ src/components/button/button.js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+import './button.scss'
+
+const ButtonCustomLink = (props) => {  
+    // Abrir modal de formulário de contato
+  return (  
+    <div className="buttonCustom_wrapper" style={{justifyContent:props.position==='left'&&'flex-start' || props.position === 'right'&&'flex-end' ||props.position === 'center'&&'center' }}> 
+    <button onClick={() => console.log('abrir modal')} className={`buttonCustom ${props.variant==='dark'?'dark':'light'}`}>
+      {props.children}
+    </button>
+    </div>
+  )
+}
+
+export default ButtonCustomLink
