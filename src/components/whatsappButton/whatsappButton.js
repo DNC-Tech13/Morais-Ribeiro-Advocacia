14e1b53999869dab975609d5a3a264f35f9f5df6
xTirian <tirianx@yahoo.com>
2024-05-05 03:05:47 -0300

    fix: ajust in whatsapp button e custom

--- src/components/whatsappButton/whatsappButton.js
+++ src/components/whatsappButton/whatsappButton.js
@@ -2,14 +2,15 @@ import Whatsapp from "../../imagens/whatsapp.svg";
 import React from 'react'
 import './whatsapp.scss'
 import Image from "next/image";
+import Link from "next/link";
 
 const WhatsappButton = () => {
+  const wppUrl = 'https://wa.me/5512991604267?text=Quero+conhecer+mais+das+as+%C3%A1reas+de+atua%C3%A7%C3%A3o.'
   return (
-    <div className='imgWhatsapp'>
-        <a href="https://wa.me/5512991604267?text=Quero+conhecer+mais+das+as+%C3%A1reas+de+atua%C3%A7%C3%A3o.">
-          <Image src={Whatsapp} />
-        </a>
-      </div>
+    <Link className='imgWhatsapp' href={wppUrl}>       
+          <Image src={Whatsapp}/>
+       
+      </Link>
   )
 }
 
