515fd2609f6c820c1b019d9f96c29030f92af67b
xTirian <tirianx@yahoo.com>
2024-05-08 23:28:28 -0300

    fix: remover logs

--- src/lib/getAreasAtuacaoByLabel.js
+++ src/lib/getAreasAtuacaoByLabel.js
@@ -6,14 +6,12 @@ dotenv.config();
 
 export const fetchAreasByLabel = async (areaLabel) => {
   const dataRef = process.env.AREAS_ATUACAO_REF || '/data/areas-de-atuacao.json';
-  console.log(dataRef)
   try {
     const response = await instance.get(dataRef, {
       cache: {
         maxAge: 15 * 60 * 1000, // 15 minutes
       },
     });    
-    console.log(areaLabel)
 
     
     return response.data.areas.filter(area => area.name === reverseSlug(areaLabel))[0];
