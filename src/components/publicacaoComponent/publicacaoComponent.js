b115a1c776c9004a19018a88adc27349cffaf36d
Luan Meira Quevedo <151397956+Luanquevedo@users.noreply.github.com>
2024-05-10 13:32:09 -0300

    Update publicacaoComponent.js

--- src/components/publicacaoComponent/publicacaoComponent.js
+++ src/components/publicacaoComponent/publicacaoComponent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./index.scss"
 
 
 const PublicacaoComponent = ({ autorInfo, publicacaoInfo }) => {
@@ -6,22 +7,29 @@ const PublicacaoComponent = ({ autorInfo, publicacaoInfo }) => {
     const { titulo, resumo, continuarLendo, data } = publicacaoInfo;
   
     return (
-      <div className="publicacao-component">
+      <div className="publicacao">
         {/* Integrando AutorCard diretamente */}
-        <div>
-          <img src={foto} alt={nome}/>
-          <h3>{nome}</h3>
-          <a href={linkMaisInfo}>Mais informações</a>
+        <div className='publicacao__autor'>
+          <div>
+            <img src={foto} alt={nome}/>
+          </div>
+          <div>
+            <h3>{nome}</h3>
+          </div>
+          <div className='publicacao__link'>
+            <a href={linkMaisInfo}>Ver mais sobre autor </a>
+            <img src='setaAutor.svg'/>
+          </div>
         </div>
   
         {/* Integrando PublicacaoCard diretamente */}
-        <div className='public'>
-          <div className='public__section1'>
+        <div className='publicacao__post'>
+          <div className='public__section'>
             <h1>{titulo}</h1>
             <p>{resumo}</p>
           </div>
-          <div className='public__section2'>
-            <div>
+          <div className='public__section1'>
+            <div className='public__section2'>
               <a href={continuarLendo}>continuar lendo</a>
               <span>{data}</span>
             </div>
