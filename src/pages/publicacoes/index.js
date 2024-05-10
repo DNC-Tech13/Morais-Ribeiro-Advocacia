01bc0f09e8f64d01aadf14165224954a1d876a23
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2024-05-10 14:27:52 -0300

    Inclusão d dos campos da page publicacoes

---
+++ src/pages/publicacoes/index.js
@@ -0,0 +1,60 @@
+
+import PublicacaoComponent from "@/components/publicacaoComponent/publicacaoComponent";
+import Header from "@/components/header/header";
+import Layout from "@/components/layout/DefaultLayout";
+import "./index.scss"
+
+const publi = () => {
+  const autor1Info = {
+    nome:'Dr. João Pedro\nRezende Ribeiro\ne Moraes de Oliveira',
+    foto:'fotocontainerjoao.svg',
+    linkMaisInfo:'inserir o url do link'
+  };
+  const autor2Info = {
+    nome:'Dra. Nikolly Morais Silva',
+    foto:'fotocontainernikolly.svg',
+    linkMaisInfo:'inserir o url do link'
+  };
+  
+  const publicacao1Info ={
+    titulo:'PJ ou CLT: Avaliando Vantagens e Desvantagens',
+    resumo:'A decisão entre trabalhar como Pessoa Jurídica (PJ) ou sob a forma da  Consolidação das Leis do Trabalho (CLT) é crucial e deve ser  cuidadosamente ponderada, levando em consideração diversos aspectos.',
+    continuarLendo:'inserir o link do artigo',
+    data:'09 de abril de 2024',
+  };
+
+  const publicacao2Info ={
+    titulo:'Exposição de Dados na Dark Web e suas Consequências Jurídicas: Um Alerta para a Segurança Digital',
+    resumo:'A internet trouxe consigo inúmeras oportunidades, mas também  desafios significativos, especialmente no que diz respeito à segurança  dos dados. Uma das áreas mais preocupantes é a chamada "Dark Web", um  segmento oculto da internet onde atividades ilícitas muitas vezes  florescem, incluindo a venda e troca de informações pessoais e  sensíveis.',
+    continuarLendo:'inserir o link do artigo',
+    data:'01 de abril de 2024',
+  };
+
+  return (
+    <>
+    <Layout>
+      <main className="view">
+        <div className="view__Banner">
+          <h1>Publicações</h1>
+        </div>
+        <div className="view__search">
+          <div className="view__info">
+            <h1>Artigos</h1>
+            <button>Entre em contato</button>
+          </div>
+          <div className="view__filter">
+            <input type="text" placeholder="Pesquisar Artigo"/>
+            <input type="text" placeholder="Filtrar por Autor"/>
+          </div>
+        </div>
+        <div className="view__container">
+          <PublicacaoComponent className="publicacao__component" autorInfo={autor1Info} publicacaoInfo={publicacao1Info} />
+          <PublicacaoComponent className="publicacao__component" autorInfo={autor2Info} publicacaoInfo={publicacao2Info} />
+        </div>
+      </main>
+    </Layout>
+    </>
+  );
+};
+
+export default publi;
